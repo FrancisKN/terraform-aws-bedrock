d17071b4f83558d40b1b87b6dab4f7d829af76dd
francis-kamga <francis.kamgangangue@consulting-for.accor.com>
2025-01-28 22:49:45 +0100

    rds_kb_identifier

--- outputs.tf
+++ outputs.tf
@@ -24,11 +24,6 @@ output "datasource_identifier" {
   description = "The unique identifier of the data source."
 }
 
-output "cloudwatch_log_group" {
-  value       = length(aws_cloudwatch_log_group.knowledge_base_cwl) > 0 ? aws_cloudwatch_log_group.knowledge_base_cwl[0].name : null
-  description = "The name of the CloudWatch log group for the knowledge base.  If no log group was requested, value will be null"
-}
-
 output "bedrock_agent" {
   value       = var.create_agent == true ? awscc_bedrock_agent.bedrock_agent : null
   description = "The Amazon Bedrock Agent if it is created."
