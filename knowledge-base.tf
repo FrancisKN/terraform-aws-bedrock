b3fe1f78d4ea4dfe4f1a467a79e768efd4128d59
Francis KN - Accor <153217709+FrancisKN@users.noreply.github.com>
2025-02-11 17:49:05 +0100

    awscc_bedrock_knowledge_base

--- knowledge-base.tf
+++ knowledge-base.tf
@@ -1,5 +1,60 @@
+# - Knowledge Base Default OpenSearch -
+resource "awscc_bedrock_knowledge_base" "knowledge_base_default" {
+  count       = var.create_default_kb ? 1 : 0
+  name        = "${var.kb_name}-${random_string.solution_prefix.result}"
+  description = var.kb_description
+  role_arn    = var.kb_role_arn != null ? var.kb_role_arn : aws_iam_role.bedrock_knowledge_base_role[0].arn
+
+  storage_configuration = {
+    type = "OPENSEARCH_SERVERLESS"
+    opensearch_serverless_configuration = {
+      collection_arn    = awscc_opensearchserverless_collection.default_collection[0].arn
+      vector_index_name = opensearch_index.default_oss_index[0].name
+      field_mapping = {
+        metadata_field = var.metadata_field
+        text_field     = var.text_field
+        vector_field   = var.vector_field
+      }
+    }
+  }
+  knowledge_base_configuration = {
+    type = "VECTOR"
+    vector_knowledge_base_configuration = {
+      embedding_model_arn = var.kb_embedding_model_arn
+    }
+  }
+  depends_on = [ time_sleep.wait_after_index_creation ]
+}
+
 # – Existing KBs –
 
+# – OpenSearch –
+resource "awscc_bedrock_knowledge_base" "knowledge_base_opensearch" {
+  count       = var.create_opensearch_config ? 1 : 0
+  name        = "${var.kb_name}-${random_string.solution_prefix.result}"
+  description = var.kb_description
+  role_arn    = var.kb_role_arn != null ? var.kb_role_arn : aws_iam_role.bedrock_knowledge_base_role[0].arn
+  tags        = var.kb_tags
+
+  storage_configuration = {
+    type = var.kb_storage_type
+    opensearch_serverless_configuration = {
+      collection_arn    = var.collection_arn
+      vector_index_name = var.vector_index_name
+      field_mapping = {
+        metadata_field = var.metadata_field
+        text_field     = var.text_field
+        vector_field   = var.vector_field
+      }
+    }
+  }
+  knowledge_base_configuration = {
+    type = var.kb_type
+    vector_knowledge_base_configuration = {
+      embedding_model_arn = var.kb_embedding_model_arn
+    }
+  }
+}
 # - Mongo –
 resource "awscc_bedrock_knowledge_base" "knowledge_base_mongo" {
   count       = var.create_mongo_config ? 1 : 0
